3d1bbdb6e3c549324087afb536426e2205526178
Onyeka Ikedinobi <ikedinobicruz7@gmail.com>
2023-02-18 11:12:23 +0100

    linting

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
