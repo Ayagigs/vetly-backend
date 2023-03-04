a47e47010b33d6cfa0358c2189b7eefbef2a62d2
gentomacine <ogwaluchidozie@gmail.com>
2023-03-04 23:20:52 +0100

    a user controller that gets the header token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,40 +1,100 @@
-
+import { StatusCodes } from "http-status-codes";
 import User from "../models/user.model";
+import UserService from "../services/user.service";
+import HttpException from "../utils/exception";
 
 
-//get all user
-
-export const fetchAllUsers = async (req, res) => {
-    try {
-        const users = await User.find({});
-        if (!users) {
-            return res.status().json({ error: "users not found" });
+export default class UserController {
+    constructor() {
+        this.UserService = new UserService();
+    }
+    async getUserById(req) {
+        const token = this.getTokenFromHeaders(req);
+        if (!token) {
+            throw new HttpException(
+                StatusCodes.BAD_REQUEST,
+                "Token not found"
+            );
         }
 
-        return res.status().json({ users });
-    } catch (error) {
-        error.message;
-    }
-};
+        try {
+            // Get the user details from the token
+            const user = await this.getUser(token);
 
-//get one user
-export const fetchOneUserById = async (req, res) => {
-    const foundUser = await User.findById();
-    try {
-        if (foundUser) {
-            res.json({
-                status: "success",
-                data: { foundUser }
-            });
-        } else {
-            res.json({
-                status: "success",
-                data: " user with such ID doesnt exist"
-            });
+            // Get the user ID from the request parameters
+            const userId = req.params.id;
+
+            // Fetch the user by ID
+            const foundUser = await User.findById(userId);
+
+            // Check if the user exists
+            if (!foundUser) {
+                throw new HttpException(
+                    StatusCodes.NOT_FOUND,
+                    "User not found"
+                );
+            }
+
+            // Check if the requesting user is authorized to access the user details
+            if (user.role !== "admin" && user._id.toString() !== foundUser._id.toString()) {
+                throw new HttpException(
+                    StatusCodes.UNAUTHORIZED,
+                    "You do not have permission to access this resource"
+                );
+            }
+
+            return foundUser;
+        } catch (error) {
+            throw new HttpException(
+                StatusCodes.INTERNAL_SERVER_ERROR,
+                "Something went wrong"
+            );
+        }
+    }
+
+
+    getTokenFromHeaders(req) {
+        const headersDetails = req.headers;
+        const token = headersDetails["authorization"]?.split(" ")[1];
+        return token;
+    }
+    async fetchAllUsers(req) {
+        const token = this.getTokenFromHeaders(req);
+        if (!token) {
+            throw new HttpException(
+                StatusCodes.BAD_REQUEST,
+                "Token not found"
+            );
         }
 
-    } catch (error) {
-        res.json(error.message);
+        try {
+            // Get the user details from the token
+            const user = await this.getUser(token);
 
+            // Check if the user has the necessary permissions to fetch all users
+            if (user.role !== "admin") {
+                throw new HttpException(
+                    StatusCodes.UNAUTHORIZED,
+                    "You do not have permission to access this resource"
+                );
+            }
+
+            // Fetch all users
+            const users = await User.find();
+
+            return users;
+        } catch (error) {
+            throw new HttpException(
+                StatusCodes.INTERNAL_SERVER_ERROR,
+                "Something went wrong"
+            );
+        }
     }
-};
+
+
+}
+
+
+
+
+
