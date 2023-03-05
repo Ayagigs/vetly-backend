1ab1335e2b11a2121e458d66b8e0f1c243f59b78
Ikedinobi Onyeka <ikedinobicruz7@gmail.com>
2023-03-05 19:32:11 +0100

    user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,23 +1,45 @@
+import User from "../models/user.model";
 import HttpException from "../utils/exception";
 import { StatusCodes } from "http-status-codes";
-import AuthService from "./auth.service";
 
-class UserService {
-    constructor() {
-        this.authService = new AuthService();
+export default class UserService {
+    
+    async list () {
+        return await User.find({});
     }
-    async register(req) {
-        try {
-            const newUser = await this.authService.register(req.body);
 
-            return newUser;
-        } catch (error) {
+    async listByRole (role) {
+        return await User.find({
+            userType: role
+        });
+    }
+
+    async search (keyword) {
+
+        return await User.find({
+            $or: [
+                {fullname: { $regex: keyword }},
+                { "local.email": { $regex: keyword } },
+                { "github.email": { $regex: keyword } },
+                { "google.email": { $regex: keyword } },
+            ],
+        });
+
+    }
+
+    async findById (id) {
+
+        const user = await User.findOne({ _id: id });
+
+        if (!user) {
             throw new HttpException(
-                error.status || StatusCodes.INTERNAL_SERVER_ERROR,
-                error.message
+                StatusCodes.NOT_FOUND,
+                "Record not found"
             );
         }
-    }
-}
 
-export default new UserService();
+        return user;
+
+    }
+
+}
