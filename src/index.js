905b4c0b9b01b9b32d3a4dd7af970fdc756b1360
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2023-03-16 22:22:32 +0100

    Included VettingRouter in app.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import App from "./app.js";
 import AuthRouter from "./auth/auth.route.js";
 import ResumeRouter from "./routes/resume.route.js";
 import UserRouter from "./routes/user.route.js";
+import VettingRouter from "./routes/vetting.route.js";
 
 function bootstrap() {
 
     const app = new App([
         new AuthRouter(),
         new ResumeRouter(),
+        new VettingRouter(),
         new UserRouter()
     ]);
 
